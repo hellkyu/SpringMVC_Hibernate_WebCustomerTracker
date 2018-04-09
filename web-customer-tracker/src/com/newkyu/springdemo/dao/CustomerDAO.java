5cf63b5b9a0298b565750b7627acaac880052894
newkyu <newkyu12@gmail.com>
2018-04-10 01:10:40 +0900

    Customer DAO and Impl

---
+++ web-customer-tracker/src/com/newkyu/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.newkyu.springdemo.dao;
+
+import java.util.List;
+
+import com.newkyu.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers(); 
+}
